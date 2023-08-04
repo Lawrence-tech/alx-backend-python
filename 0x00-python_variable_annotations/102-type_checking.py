97070809bf0078800ffa3601374d60bac55d57c9
Lawrence-tech <lawrencesagini2@gmail.com>
2023-08-05 00:04:26 +0300

    some changes

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,53 +1,19 @@
 #!/usr/bin/env python3
-
-"""
-Type checking
-"""
-
-from typing import Tuple, Any
+'''Task 12's module.
+'''
+from typing import List, Tuple
 
 
-def zoom_array(lst: Tuple[Any, ...], factor: int = 2) -> Tuple[Any, ...]:
-    """
-    Zooms in on the elements of a given list by repeating each element multiple
-    times.
-
-    This function takes a tuple 'lst' and an optional integer 'factor'.
-    It creates a new list where each element from 'lst' is repeated 'factor'
-    times.
-
-    Parameters:
-        lst (Tuple[Any, ...]): The input tuple containing elements to be zoomed
-        in.
-        factor (int, optional): The factor by which each element should be
-        repeated. Defaults to 2.
-
-    Returns:
-        Tuple[Any, ...]: A new tuple containing elements from 'lst' repeated
-        'factor'
-        times.
-
-    Example:
-        >>> array = (12, 72, 91)
-        >>> zoom_2x = zoom_array(array)
-        >>> print(zoom_2x)
-        (12, 12, 72, 72, 91, 91)
-
-        >>> zoom_3x = zoom_array(array, 3)
-        >>> print(zoom_3x)
-        (12, 12, 12, 72, 72, 72, 91, 91, 91)
-    """
-    zoomed_in = [
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    '''Creates multiple copies of items in a tuple.
+    '''
+    zoomed_in: List = [
         item for item in lst
-        for i in range(factor)
+        for i in range(int(factor))
     ]
-    return tuple(zoomed_in)
+    return zoomed_in
 
 
 array = (12, 72, 91)
 
-# Zoom the array by a factor of 2 (default)
 zoom_2x = zoom_array(array)
-
-# Zoom the array by a factor of 3
-zoom_3x = zoom_array(array, 3)
