7609edcefb06e6a72cc185770178794fe3da5705
Lawrence-tech <lawrencesagini2@gmail.com>
2023-08-13 10:42:28 +0300

    Async Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,7 +7,7 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None, None]:
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     Asynchronous generator that yields random numbers between 0 and 10.
 
